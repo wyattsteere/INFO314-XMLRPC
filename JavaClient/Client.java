2000f54efc5f01d326e613592360f15548bae08f
Wyatt Steere <94156893+wyattsteere@users.noreply.github.com>
2023-05-11 19:08:52 -0700

    cannot receive response from server although server sends

--- JavaClient/Client.java
+++ JavaClient/Client.java
@@ -1,3 +1,5 @@
+package edu.uw.info314.xmlrpc.server;
+
 import java.io.*;
 import java.net.*;
 import java.net.http.*;
@@ -12,9 +14,16 @@ import org.w3c.dom.Node;
  * were introduced in Java11.
  */
 public class Client {
+    public static String uri = "";
+
     private static DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 
     public static void main(String... args) throws Exception {
+        String host = args[0];
+        int port = Integer.valueOf(args[1]);
+
+        uri = "http://" + host + ":" + port + "/RPC";
+//        System.out.println("***");
         System.out.println(add() == 0);
         System.out.println(add(1, 2, 3, 4, 5) == 15);
         System.out.println(add(2, 4) == 6);
@@ -25,24 +34,62 @@ public class Client {
         System.out.println(modulo(10, 5) == 0);
     }
     public static int add(int lhs, int rhs) throws Exception {
-        return -1;
+        return sendReq("add", lhs, rhs);
     }
-    public static int add(Integer... params) throws Exception {
-        return -1;
+    public static int add(Integer... args) throws Exception {
+        return sendReq("add", (Object[])args);
     }
     public static int subtract(int lhs, int rhs) throws Exception {
-        return -1;
+        return sendReq("subtract", lhs, rhs);
     }
     public static int multiply(int lhs, int rhs) throws Exception {
-        return -1;
+        return sendReq("multiply", lhs, rhs);
     }
-    public static int multiply(Integer... params) throws Exception {
-        return -1;
+    public static int multiply(Integer... args) throws Exception {
+        return sendReq("multiply", (Object[])args);
     }
     public static int divide(int lhs, int rhs) throws Exception {
-        return -1;
+        return sendReq("divide", lhs, rhs);
     }
     public static int modulo(int lhs, int rhs) throws Exception {
-        return -1;
+        return sendReq("modulo", lhs, rhs);
+    }
+
+    public static int sendReq(String calcOp, Object... args) throws Exception {
+        String params = "<params>";
+        for (Object arg : args) {
+            if (arg instanceof Integer) {
+                params+= "<param><value><i4>" +arg + "</i4></value></param>";
+            } else if (arg instanceof String) {
+                params+= "<param><value><string>" + arg + "</string></value></param>";
+            }
+        }
+        params +="</params>";
+
+        String reqBody = "<?xml version='1.0'?><methodCall><methodName>" + calcOp + "</methodName><params>";
+        reqBody += params + "</params></methodCall>";
+
+        HttpRequest httpReq = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .header("Content-Type", "text/xml")
+                .POST(HttpRequest.BodyPublishers.ofString(reqBody))
+                .build();
+
+        HttpResponse<String> httpResp = HttpClient.newHttpClient().send(httpReq, HttpResponse.BodyHandlers.ofString());
+        return responseHandler(httpResp.body());
+    }
+
+    public static int responseHandler(String response) throws Exception {
+        DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+
+        ByteArrayInputStream input =  new ByteArrayInputStream(
+                response.getBytes("UTF-8"));
+        Document doc = docBuilder.parse(input);
+
+        XPath xPath =  XPathFactory.newInstance().newXPath();
+        String expression = "/methodResponse/params/param/value/i4";
+        Node node = (Node) xPath.compile(expression).evaluate(doc, XPathConstants.NODE);
+        System.out.println(Integer.parseInt(node.getTextContent()));
+        return Integer.parseInt(node.getTextContent());
     }
 }
